60ca337d97811a18cb4bd11ef1369915dc8e62a3
Clayder <peterclayder@gmail.com>
2018-01-09 16:49:56 -0200

    Componentes Vue: Criei os componentes Replies e threads.

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -7,4 +7,4 @@
 <script src="/js/manifest.js"></script>
 <script src="/js/vendor.js"></script>
 <script src="/js/bootstrap.js"></script>
-<script src="/js/app.js"></script>
+<script src="/js/threads.js"></script>
